ee2a885b9f437b2a3ab9fd82cb0cd925efd5d90a
jpOlivo <juam17@gmail.com>
2019-08-02 23:08:55 -0300

    update ms-mail

--- ms-mail/src/main/java/com/nu/service/EmailServiceImpl.java
+++ ms-mail/src/main/java/com/nu/service/EmailServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import com.nu.domain.Mail;
 import com.nu.dto.UserDTO;
 import com.nu.repository.MailRepository;
 
+@Service
 public class EmailServiceImpl implements EmailService {
 	@Autowired
 	private JavaMailSender emailSender;
